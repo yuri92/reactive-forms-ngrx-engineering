00a1e281340307fdc629b3f6539440864e7851c3
Yuri Simeone <y.simeone@nesea.it>
2023-09-23 15:25:36 +0200

    Subject e BehaviourSubject

--- src/app/features/observables/observables.component.ts
+++ src/app/features/observables/observables.component.ts
@@ -1,6 +1,5 @@
-import {Component, inject, OnDestroy, OnInit} from '@angular/core';
-import {Observable, of, Subscription, tap, throwError} from 'rxjs';
-import {catchError, finalize} from 'rxjs/operators';
+import {Component, inject, OnInit} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
 import {SharedService} from "../../shared/shared.service";
 
 @Component({
@@ -10,26 +9,48 @@ import {SharedService} from "../../shared/shared.service";
 })
 export class ObservablesComponent implements OnInit {
 
-    value$: Observable<string>;
-    value2$: Observable<string>;
+    value$: Observable<number>;
+    valueBehaviorSubject$: Observable<number>;
     service = inject(SharedService)
 
     constructor() {
     }
 
     ngOnInit() {
-        const customObservable: Observable<string> = new Observable((subscriber) => {
-            for (let i = 0; i < 1000; i++) {
-                setTimeout(() => {
-                    subscriber.next(`Emesso valore ${i}`);
-                }, i * 1000)
-            }
-        });
+        // const customObservable: Observable<string> = new Observable((subscriber) => {
+        //     for (let i = 0; i < 1000; i++) {
+        //         setTimeout(() => {
+        //             subscriber.next(`Emesso valore ${i}`);
+        //         }, i * 1000)
+        //     }
+        // });
+        //
+        // this.value$ = customObservable;
+        // setTimeout(() => {
+        //     this.value2$ = customObservable;
+        // }, 3000)
 
-        this.value$ = customObservable;
-        setTimeout(() => {
-            this.value2$ = customObservable;
-        }, 3000)
+        // const subject: Subject<string> = new Subject();
+        //
+        // let i = 0;
+        // setInterval(() => {
+        //     subject.next('Valore ' + i++)
+        // }, 1000)
+        //
+        // subject.subscribe(value => console.log('Observer Yuri', value))
+        //
+        // setTimeout(() => {
+        //     subject.subscribe(value => console.log('Observer Andrea', value))
+        // }, 3000)
+
+        this.value$ = this.service.valueSubject$;
+        this.valueBehaviorSubject$ = this.service.valueBehaviorSubject$;
+
+
+
+        this.service.valueSubject$.subscribe(console.log)
+
+
     }
 
 
--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,27 @@ export class SharedService {
 
   public helloWorldString = 'Hello world';
 
+  private valueSubject: Subject<number> = new Subject();
+  public valueSubject$ = this.valueSubject.asObservable();
+
+  private valueBehaviorSubject = new BehaviorSubject<number>(0);
+  public valueBehaviorSubject$ = this.valueBehaviorSubject.asObservable();
+
   constructor() {
     let i = 0;
     setInterval(() => {
       this.helloWorldString = `Hello world ${i++}`;
+    }, 1000)
+
+    let j = 0;
+    setInterval(() => {
+      this.valueSubject.next(j++)
+    }, 1000)
+
+
+    let z = 0;
+    setInterval(() => {
+      this.valueBehaviorSubject.next(z++)
     }, 1000)
   }
 }
